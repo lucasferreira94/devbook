9b29c54fe835823c1cbb8f71796a4a7f09d97d52
lucasferreira94 <lucas_sferreira94@hotmail.com>
2024-02-13 16:16:49 -0300

    criado package router 1

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module github.com/lucasferreira94/devbook/api.git
 
 go 1.21.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- api/main.go
+++ api/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("rodando api")
+	r := router.Gerar()
+
+	log.Fatal(http.ListenAndServe(":5000", r))
 }
